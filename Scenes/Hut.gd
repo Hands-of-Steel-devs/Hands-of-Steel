704fdfee857aecaece78a4674fbdc886c9c32ae1
BadProgram_exe <lau12casapu@gmail.com>
2021-10-22 18:04:16 +0300

    Update 3.1
    Added hut updated

--- Scenes/Hut.gd
+++ Scenes/Hut.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var sprite = $Sprite
 onready var button = $Button
 onready var sprite_finish = $Sprite_finished
+onready var Text = $RichTextLabel
 
 func _ready():
 	sprite_finish.visible = false
@@ -10,4 +11,5 @@ func _ready():
 func _on_Button_pressed():
 	sprite.visible = false
 	button.visible = false
+	Text.visible = false
 	sprite_finish.visible = true
