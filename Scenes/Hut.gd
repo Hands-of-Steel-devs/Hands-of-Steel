42a9b8fbb4baca26db0b8f055b7326aaaa04418b
BadProgram_exe <lau12casapu@gmail.com>
2021-10-22 17:57:11 +0300

    Update 3
    hut done and more shit

--- Scenes/Hut.gd
+++ Scenes/Hut.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 onready var sprite = $Sprite
 onready var button = $Button
+onready var sprite_finish = $Sprite_finished
 
 func _ready():
+	sprite_finish.visible = false
+
+func _on_Button_pressed():
+	sprite.visible = false
 	button.visible = false
+	sprite_finish.visible = true
--- Scenes/Main menu.gd
+++ Scenes/Main menu.gd
@@ -7,7 +7,8 @@ func _on_Start_pressed():
 	get_tree().change_scene("res://Scenes/World.tscn")
 
 func _on_Continue_pressed():
-	pass # Replace with function body.
+	pass
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
