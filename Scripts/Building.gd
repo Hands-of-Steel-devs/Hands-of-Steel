1b3f5362f263c7229806041138d49c7b0e71f055
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-10-22 23:01:28 +0700

    Cache nodes to (hopefully) improve speed

--- Scripts/Building.gd
+++ Scripts/Building.gd
@@ -3,30 +3,40 @@ class_name Building
 
 export var building_name: String
 export var price: int
-export var building_label: NodePath
-export var unlock_button: NodePath
 
-export var locked_sprite: NodePath
-export var unlocked_sprite: NodePath
+export var building_label_node: NodePath
+export var unlock_button_node: NodePath
+export var locked_sprite_node: NodePath
+export var unlocked_sprite_node: NodePath
+
+var building_label
+var unlock_button
+var locked_sprite
+var unlocked_sprite
 
 signal building_unlocked
 
 func _ready() -> void:
-	# TODO: Complain to Godot engine devs about not having lambda functions :)
-	get_node(building_label).text = "%s - %s" % [building_name, _get_price_string()]
-	get_node(unlock_button).text = "Unlock"
-	get_node(unlock_button).connect("pressed", self, "_on_unlock_button_pressed")
+	building_label = get_node(building_label_node)
+	unlock_button = get_node(unlock_button_node)
+	locked_sprite = get_node(locked_sprite_node)
+	unlocked_sprite = get_node(unlocked_sprite_node)
 	
-	get_node(locked_sprite).visible = true
-	get_node(unlocked_sprite).visible = false
+	# TODO: Complain to Godot engine devs about not having lambda functions :)
+	building_label.text = "%s - %s" % [building_name, _get_price_string()]
+	unlock_button.text = "Unlock"
+	unlock_button.connect("pressed", self, "_on_unlock_button_pressed")
+	
+	locked_sprite.visible = true
+	unlocked_sprite.visible = false
 
 func _on_unlock_button_pressed() -> void:
-	get_node(unlock_button).visible = false
-	get_node(building_label).visible = false
+	unlock_button.visible = false
+	building_label.visible = false
 	emit_signal("building_unlocked")
 	
-	get_node(locked_sprite).visible = false
-	get_node(unlocked_sprite).visible = true
+	locked_sprite.visible = false
+	unlocked_sprite.visible = true
 
 func _get_price_string() -> String:
 	if price > 0:
